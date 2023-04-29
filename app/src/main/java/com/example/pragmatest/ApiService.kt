57a5125ac49162649e5c093d25a9feafccb617d1
andresorjuela <ing.jaorjuela@gmail.com>
2023-04-28 20:36:51 -0500

    adding service model in order to provide service also adding getCats method in ApiService interface.

--- app/src/main/java/com/example/pragmatest/ApiService.kt
+++ app/src/main/java/com/example/pragmatest/ApiService.kt
@@ -1,10 +1,17 @@
 package com.example.pragmatest
 
+import retrofit2.Response
+import retrofit2.http.Header
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
 interface ApiService {
 
     companion object{
-        const val BASE_URL = "https://dev.obtenmas.com/catom/api/"
+        const val BASE_URL = "https://api.thecatapi.com/v1/"
     }
 
-
+    @Headers("x-api-key: bda53789-d59e-46cd-9bc4-2936630fde39")
+    @POST("breeds")
+    suspend fun getCatList(): Response<List<Cat>>
 }
---
+++ app/src/main/java/com/example/pragmatest/ServiceModule.kt
@@ -0,0 +1,37 @@
+package com.example.pragmatest
+
+import com.facebook.stetho.okhttp3.StethoInterceptor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class ServiceModule {
+
+    @Provides
+    @Singleton
+    fun provideClient(): OkHttpClient =
+        OkHttpClient.Builder()
+            .addNetworkInterceptor(StethoInterceptor())
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(client: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(ApiService.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideGenericApi(retrofit: Retrofit): ApiService =
+        retrofit.create(ApiService::class.java)
+}
